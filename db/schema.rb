0a93c974b2159dd898a01dfcb9a91f8ba55274fc
Evgeny Pivovarov <geny.pivo@gmail.com>
2017-01-19 10:31:04 +0200

    Added event time to events table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170119053436) do
+ActiveRecord::Schema.define(version: 20170119082954) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 20170119053436) do
     t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "event_time"
   end
 
   add_index "events", ["user_id"], name: "index_events_on_user_id", using: :btree
