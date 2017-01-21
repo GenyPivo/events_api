57b0bd3722c3d395bbd5f3af3f23cd2301752dc3
Evgeny Pivovarov <geny.pivo@gmail.com>
2017-01-21 11:00:58 +0200

    Fit invites controller to spec

--- app/controllers/api/invites_controller.rb
+++ app/controllers/api/invites_controller.rb
@@ -1,14 +1,28 @@
 class Api::InvitesController < ApplicationController
   before_action :doorkeeper_authorize!
+
+  DELETE_MESSAGE = 'Invite deleted'.freeze
   
   def index
-
+    invites = current_user.events.find(params[:event_id]).invites
+    render json: request_success(invites)
   end
 
   def create
+    event = current_user.events.find(params[:event_id])
+    invite = event.invites.new(invites_params)
+    render json: request_success(invite), status: CREATED if invite.save!
   end
 
   def destroy
+    current_user.invites.delete(params[:id])
+    render json: request_success(DELETE_MESSAGE), status: NO_CONTENT
+  end
+
+  private
+
+  def invites_params
+    params.permit(:invited_user_id)
   end
 end
 
--- spec/controllers/invites_controller_spec.rb
+++ spec/controllers/invites_controller_spec.rb
@@ -1,10 +1,11 @@
 require 'rails_helper'
 
-RSpec.describe InvitesController, type: :controller do
+RSpec.describe Api::InvitesController, type: :controller do
   include_context "shared oauth"
   let!(:invited_user) { create(:user, email: 'sample@sample.net') }
   let!(:invited_user2) { create(:user, email: 'sample2@sample.net') }
   let!(:event) { create(:event, user_id: user.id) }
+  let!(:model) { Invite }
   let!(:invite) do
     create(:invite, user_id: user.id, invited_user_id: invited_user.id, event_id: event.id)
   end
