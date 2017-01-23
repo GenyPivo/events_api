5af81fe6d4b43ba3df4597a2081709ff142eca74
Evgeny Pivovarov <geny.pivo@gmail.com>
2017-01-23 15:42:13 +0200

    User controller spec

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
-RSpec.describe UsersController, type: :controller do
-
+RSpec.describe Api::UsersController, type: :controller do
+  let!(:model) { User }
+  it_behaves_like 'record create' do
+    let!(:user) do
+      {
+          user: {
+              email: 'exx@sample.com',
+              password: 'password'
+          }
+      }
+    end
+    let!(:params) { {query: user} }
+  end
 end
